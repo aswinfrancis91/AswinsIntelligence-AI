71df951ee05c2d5cf57cb545f87c0da392af5c77
Aswin Francis <afrancis@gsiccorp.net>
2025-05-30 15:27:22 +0530

    Remove unused import and fix formatting in `AppHost.cs`

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,5 +1,3 @@
-using YamlDotNet.Core.Tokens;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.API>("AswinsIntelligenceApi");
@@ -7,7 +5,7 @@ var api = builder.AddProject<Projects.API>("AswinsIntelligenceApi");
 var website = builder.AddNpmApp("react", "../ChatInterface", "dev")
     .WithReference(api)
     .WaitFor(api)
-    .WithHttpEndpoint(env: "VITE_PORT",port: 5173)
+    .WithHttpEndpoint(env: "VITE_PORT", port: 5173)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
